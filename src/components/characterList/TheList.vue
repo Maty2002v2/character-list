ef278adf515a3430a1869a55af70d8257cbb48ca
maty <matikster1@wp.pl>
2022-08-31 18:02:03 +0200

    Create a base list component template

---
+++ src/components/characterList/TheList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="character-list">
+    <div class="character-list__list">
+      <list-item
+        v-for="(character, index) in characterData"
+        :key="index"
+        :character="character"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import { storeToRefs } from "pinia";
+
+import ListItem from "./ListItem.vue";
+
+import { useCharacterApiStore } from "../../stores/CharacterApiStore";
+
+export default defineComponent({
+  name: "TheList",
+  components: {
+    ListItem,
+  },
+  async setup() {
+    const { characterData } = storeToRefs(useCharacterApiStore());
+    const { fetchData } = useCharacterApiStore();
+
+    await fetchData();
+
+    return { characterData };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.character-list {
+  &__list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    gap: 50px;
+    padding: 40px;
+    box-sizing: border-box;
+  }
+}
+</style>
